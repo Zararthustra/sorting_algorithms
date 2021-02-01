567bc2f5f318ba7e243a2741297f29b1a7e91a96
Arthur <arthmayer@outlook.fr>
2021-02-01 09:49:29 +0000

    add q1 test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,5 +10,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *compare;
 
+	if (list == NULL)
+		return;
+
+	compare = *list;
+	compare = compare->next;
+	while (compare)
+	{
+		while (compare->prev != NULL && compare->prev->n > compare->n)
+		{
+			compare->prev->next = compare->next;
+
+			if (compare->next)
+				compare->next->prev = compare->prev;
+
+			compare->next = compare->prev;
+			compare->prev = compare->next->prev;
+			compare->next->prev = compare;
+
+			if (!compare->prev)
+				*list = compare;
+			else
+				compare->prev->next = compare;
+			print_list(*list);
+		}
+		compare = compare->next;
+	}
 }
