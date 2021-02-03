6644a8fa76eb684c1e2027c802d6114d006f948a
Arthur <arthmayer@outlook.fr>
2021-02-03 23:44:29 +0000

    cleaning

--- sort.h
+++ sort.h
@@ -28,9 +28,11 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+
+/* counting sort func */
 int get_max_value(int *array, size_t size);
 int *create_index_count_array(int *array, size_t size, int max_val);
 int *copy_array(int *array, size_t size);
-void radix_sort(int *array, size_t size);
 void counting_sort_idx(int *array, size_t size, int i);
+
 #endif
